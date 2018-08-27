df28318561714d4a0183eda6fc0ad96e74221eff
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-08-27 00:55:06 -0400

    added ability to display multiple notes

--- src/Board.js
+++ src/Board.js
@@ -1,14 +1,44 @@
 import React, { Component} from 'react'
-import Note from '/.Note'
+import Note from './Note'
 
 class Board extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: [
+                {
+                    id: 1,
+                    note: "Learn ReactJs"
+                },
+                {
+                    id: 2,
+                    note: "Learn React Redux"
+                },
+                {
+                    id: 3,
+                    note: "Find alternative to Lynda"
+                }
+            ]
+        }
+        this.eachNote = this.eachNote.bind(this)
+    }
+
+    eachNote(note, i) {
+        return (
+            <Note key={i}
+                index={i}>
+                {note.note}
+            </Note>
+        )
+    }
+
     render() {
         return (
             <div className="board">
-                <Note></Note>
+                {this.state.notes.map(this.eachNote)}
             </div>
         )
     }
 }
 
-export default Board.js
+export default Board
